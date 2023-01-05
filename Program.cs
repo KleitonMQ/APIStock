cbd845f478cecacdf972c467b46a6a9222fc886d
Kleiton MQ <kleiton.x5@gmail.com>
2023-01-04 21:04:49 -0300

    ciracao do banco de dados e correcao de problemas com a conectionstring

--- Program.cs
+++ Program.cs
@@ -1,6 +1,11 @@
+using APIStock.src.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<DataBaseContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("StandardConection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
--- src/Context/DataBaseContext.cs
+++ src/Context/DataBaseContext.cs
@@ -7,8 +7,17 @@ using APIStock.src.Entities;
 
 namespace APIStock.src.Context
 {
-    public class DataBaseContext
+    public class DataBaseContext : DbContext
     {
-        
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Product> Products{get; set;}
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Product>().HasKey(key => key.ID);
+        }
     }
 }
